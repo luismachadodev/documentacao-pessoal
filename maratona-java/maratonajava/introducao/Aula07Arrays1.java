a46e93ff9e0064e716c09908b510f219738039d9
Luis Henrique <luis@prover.net.br>
2025-02-28 02:01:07 -0300

    Organizando arquivos dentro da pasta maratona-java

---
+++ maratona-java/maratonajava/introducao/Aula07Arrays1.java
@@ -0,0 +1,13 @@
+package maratonajava.introducao;
+
+public class Aula07Arrays1 {
+    public static void main(String[] args) {
+        int[] idades = new int[3];
+        idades[0] = 21;
+        idades[1] = 18;
+        idades[2] = 25;
+        System.out.println(idades[0]);
+        System.out.println(idades[1]);
+        System.out.println(idades[2]);
+    }
+}
