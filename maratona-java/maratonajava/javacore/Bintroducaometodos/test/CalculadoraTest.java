077d91cf4e3a7d7c73eb5a33a207264bd0a06923
Luis Henrique <luis@prover.net.br>
2025-03-04 02:48:57 -0300

    feature: aula 44 - metodos pt1

---
+++ maratona-java/maratonajava/javacore/Bintroducaometodos/test/CalculadoraTest.java
@@ -0,0 +1,12 @@
+package maratonajava.javacore.Bintroducaometodos.test;
+
+import maratonajava.javacore.Bintroducaometodos.dominio.Calculadora;
+
+public class CalculadoraTest {
+    public static void main(String[] args) {
+        Calculadora calculadora = new Calculadora();
+        calculadora.somaDoisNumeros();
+        calculadora.subtraiDoisNumeros();
+        System.out.println("Finalizando CalculadoraTest");
+    }
+}
