7abf65de31aca28e60c13dddeecb46983818eeb6
Luis Henrique <luis@prover.net.br>
2025-04-01 07:26:32 -0300

    feat: aula 54 - get e set

---
+++ maratona-java/maratonajava/javacore/Bintroducaometodos/test/PessoaTest.java
@@ -0,0 +1,11 @@
+package maratonajava.javacore.Bintroducaometodos.test;
+
+import maratonajava.javacore.Bintroducaometodos.dominio.Pessoa;
+
+public class PessoaTest {
+    public static void main(String[] args) {
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Jiraya");
+        pessoa.setIdade(70);
+    }
+}
