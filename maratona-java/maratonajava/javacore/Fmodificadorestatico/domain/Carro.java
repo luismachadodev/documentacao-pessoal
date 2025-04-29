bd8c12e69d1d219ef3aed9b228cafa93f3540b5e
Luis Henrique <luis@prover.net.br>
2025-04-29 07:15:44 -0300

    feat: metodos estaticos

--- maratona-java/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
+++ maratona-java/maratonajava/javacore/Fmodificadorestatico/domain/Carro.java
@@ -3,7 +3,7 @@ package maratonajava.javacore.Fmodificadorestatico.domain;
 public class Carro {
     private String nome;
     private double velocidadeMaxima;
-    public static double velocidadeLimite = 250;
+    private static double velocidadeLimite = 250;
 
     public Carro(String nome, double velocidadeMaxima) {
         this.nome = nome;
@@ -12,10 +12,20 @@ public class Carro {
 
     public void imprime() {
         System.out.println("---------------");
-        System.out.println("Nome "+this.nome);
+        System.out.println("Nome " + this.nome);
         System.out.println("Velocidade máxima "+ this.velocidadeMaxima);
         System.out.println("Velocidade limite "+ Carro.velocidadeLimite);
     }
+
+    public static void setVelocidadeLimite(double velocidadeLimite) {
+        Carro.velocidadeLimite = velocidadeLimite;
+    }
+
+    public static double getVelocidadeLimite() {
+        return Carro.velocidadeLimite;
+    }
+
+
 
     public String getNome() {
         return nome;
