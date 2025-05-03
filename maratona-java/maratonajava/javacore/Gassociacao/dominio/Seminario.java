c44f1d09ccad1c46aaaf1b1b80f06c52b8c3f05a
Luis Henrique <luis@prover.net.br>
2025-05-03 19:07:42 -0300

    feat: inicio heranca

--- maratona-java/maratonajava/javacore/Gassociacao/dominio/Seminario.java
+++ maratona-java/maratonajava/javacore/Gassociacao/dominio/Seminario.java
@@ -5,19 +5,15 @@ public class Seminario {
     private Aluno[] alunos;
     private Local local;
 
-    public Seminario(String titulo) {
+    public Seminario(String titulo, Local local) {
         this.titulo = titulo;
+        this.local = local;
     }
 
     public Seminario(String titulo, Aluno[] alunos, Local local) {
         this.titulo = titulo;
         this.alunos = alunos;
         this.local = local;
-    }
-
-    public Seminario(String titulo, Aluno[] alunos) {
-        this.titulo = titulo;
-        this.alunos = alunos;
     }
 
     public Local getLocal() {
---
+++ maratona-java/maratonajava/javacore/Hheranca/test/HerancaTest.java
@@ -0,0 +1,27 @@
+package maratonajava.javacore.Hheranca.test;
+
+import maratonajava.javacore.Hheranca.dominio.Endereco;
+import maratonajava.javacore.Hheranca.dominio.Funcionario;
+import maratonajava.javacore.Hheranca.dominio.Pessoa;
+
+public class HerancaTest {
+    public static void main(String[] args) {
+        Endereco endereco = new Endereco();
+        endereco.setRua("Rua 3");
+        endereco.setCep("012345-209");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("Luis Henrique");
+        pessoa.setCpf("111111");
+        pessoa.setEndereco(endereco);
+        pessoa.imprime();
+
+        System.out.println("-----------");
+
+        Funcionario funcionario = new Funcionario();
+        funcionario.setNome("Kassadin");
+        funcionario.setCpf("666");
+        funcionario.setEndereco(endereco);
+        funcionario.setSalario(20000);
+        funcionario.imprime();
+    }
+}
