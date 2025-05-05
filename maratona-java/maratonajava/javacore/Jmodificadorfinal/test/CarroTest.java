f374834f9b04001a98afa3488f2f364f6c4cfee7
Luis Henrique <luis@prover.net.br>
2025-05-05 02:17:30 -0300

    feat: modificador final - Tipo referência

--- maratona-java/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
+++ maratona-java/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -1,10 +1,15 @@
 package maratonajava.javacore.Jmodificadorfinal.test;
 
 import maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+import maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
 
 public class CarroTest {
     public static void main(String[] args) {
         Carro carro = new Carro();
+
         System.out.println(Carro.VELOCIDADE_LIMITE);
+        System.out.println(carro.COMPRADOR);
+        carro.COMPRADOR.setNome("Abel");
+        System.out.println(carro.COMPRADOR);
     }
 }
