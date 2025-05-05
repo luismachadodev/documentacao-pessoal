2c66958103d63f013cd77bf406d0821479480073
Luis Henrique <luis@prover.net.br>
2025-05-05 02:25:31 -0300

    feat: modificador final - Classes e métodos

--- maratona-java/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
+++ maratona-java/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -2,6 +2,7 @@ package maratonajava.javacore.Jmodificadorfinal.test;
 
 import maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
 import maratonajava.javacore.Jmodificadorfinal.dominio.Comprador;
+import maratonajava.javacore.Jmodificadorfinal.dominio.Ferrari;
 
 public class CarroTest {
     public static void main(String[] args) {
@@ -11,5 +12,8 @@ public class CarroTest {
         System.out.println(carro.COMPRADOR);
         carro.COMPRADOR.setNome("Abel");
         System.out.println(carro.COMPRADOR);
+        Ferrari ferrari = new Ferrari();
+        ferrari.setNome("Enzo");
+        ferrari.imprime();
     }
 }
