fc22d9189e76ea11086e2a141841c5051ad887a5
Luis Henrique <luis@prover.net.br>
2025-05-05 01:52:17 -0300

    feat: sobrescrita toString

---
+++ maratona-java/maratonajava/javacore/Isobrescrita/test/AnimeTest.java
@@ -0,0 +1,10 @@
+package maratonajava.javacore.Isobrescrita.test;
+
+import maratonajava.javacore.Isobrescrita.dominio.Anime;
+
+public class AnimeTest {
+    public static void main(String[] args) {
+        Anime anime = new Anime("Solo Leveling");
+        System.out.println(anime);
+    }
+}
