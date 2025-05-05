d57ce2f947926d94c1aa17f3971b1ec05431df63
Luis Henrique <luis@prover.net.br>
2025-05-05 02:09:25 -0300

    feat: modificador final - Tipo primitivo

---
+++ maratona-java/maratonajava/javacore/Jmodificadorfinal/test/CarroTest.java
@@ -0,0 +1,10 @@
+package maratonajava.javacore.Jmodificadorfinal.test;
+
+import maratonajava.javacore.Jmodificadorfinal.dominio.Carro;
+
+public class CarroTest {
+    public static void main(String[] args) {
+        Carro carro = new Carro();
+        System.out.println(Carro.VELOCIDADE_LIMITE);
+    }
+}
