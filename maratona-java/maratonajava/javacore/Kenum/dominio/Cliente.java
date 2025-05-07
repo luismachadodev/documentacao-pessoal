7695715e8687f500ffdaa7e6897a959462929108
Luis Henrique <luis@prover.net.br>
2025-05-07 08:29:10 -0300

    feat: enum - sobrescrita de metodos

--- maratona-java/maratonajava/javacore/Kenum/dominio/Cliente.java
+++ maratona-java/maratonajava/javacore/Kenum/dominio/Cliente.java
@@ -1,9 +1,6 @@
 package maratonajava.javacore.Kenum.dominio;
 
 public class Cliente {
-    public enum TipoPagamento {
-        DEBITO, CREDITO
-    }
     private String nome;
     private TipoCliente tipoCliente;
     private TipoPagamento tipoPagamento;
--- maratona-java/maratonajava/javacore/Kenum/test/ClienteTest.java
+++ maratona-java/maratonajava/javacore/Kenum/test/ClienteTest.java
@@ -2,12 +2,16 @@ package maratonajava.javacore.Kenum.test;
 
 import maratonajava.javacore.Kenum.dominio.Cliente;
 import maratonajava.javacore.Kenum.dominio.TipoCliente;
+import maratonajava.javacore.Kenum.dominio.TipoPagamento;
 
 public class ClienteTest {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Luis", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
-        Cliente cliente2 = new Cliente("Dora", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
+        Cliente cliente1 = new Cliente("Luis", TipoCliente.PESSOA_FISICA, TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Dora", TipoCliente.PESSOA_JURIDICA, TipoPagamento.CREDITO);
+
         System.out.println(cliente1);
         System.out.println(cliente2);
+        System.out.println(TipoPagamento.DEBITO.calcularDesconto(100));
+        System.out.println(TipoPagamento.CREDITO.calcularDesconto(100));
     }
 }
