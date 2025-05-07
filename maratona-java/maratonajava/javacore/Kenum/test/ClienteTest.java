3f1dcd17d465ea2c53adfa84695d79f5bead3e8b
Luis Henrique <luis@prover.net.br>
2025-05-07 08:16:45 -0300

    feat: enum - construtores e atributos

--- maratona-java/maratonajava/javacore/Kenum/test/ClienteTest.java
+++ maratona-java/maratonajava/javacore/Kenum/test/ClienteTest.java
@@ -5,11 +5,9 @@ import maratonajava.javacore.Kenum.dominio.TipoCliente;
 
 public class ClienteTest {
     public static void main(String[] args) {
-        Cliente cliente1 = new Cliente("Luis", TipoCliente.PESSOA_FISICA);
-        Cliente cliente2 = new Cliente("Dora", TipoCliente.PESSOA_FISICA);
-        Cliente cliente3 = new Cliente("Dora", TipoCliente.PESSOA_JURIDICA);
+        Cliente cliente1 = new Cliente("Luis", TipoCliente.PESSOA_FISICA, Cliente.TipoPagamento.DEBITO);
+        Cliente cliente2 = new Cliente("Dora", TipoCliente.PESSOA_JURIDICA, Cliente.TipoPagamento.CREDITO);
         System.out.println(cliente1);
         System.out.println(cliente2);
-        System.out.println(cliente3);
     }
 }
