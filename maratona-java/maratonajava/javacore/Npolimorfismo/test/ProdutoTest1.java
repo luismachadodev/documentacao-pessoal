793d961f2c601754eba3c76399a1bb813db7e36a
Luis Henrique <luis@prover.net.br>
2025-05-15 08:53:43 -0300

    feat: polimorfismo - introducao

---
+++ maratona-java/maratonajava/javacore/Npolimorfismo/test/ProdutoTest1.java
@@ -0,0 +1,15 @@
+package maratonajava.javacore.Npolimorfismo.test;
+
+import maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest1 {
+    public static void main(String[] args) {
+        Computador computador = new Computador("NUC10i7", 11000);
+        Tomate tomate = new Tomate("Tomate Gauderio", 10);
+        CalculadoraImposto.calcularImpostoComputador(computador);
+        System.out.println("---------------");
+        CalculadoraImposto.calcularImpostoTomate(tomate);
+    }
+}
