191311924a2f186b60cf89868548409301769f61
Luis Henrique <luis@prover.net.br>
2025-05-15 07:33:30 -0300

    feat: interfaces - introducao

---
+++ maratona-java/maratonajava/javacore/Minterfaces/dominio/DatabaseLoader.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.Minterfaces.dominio;
+
+public class DatabaseLoader implements DataLoader {
+    @Override
+    public void load() {
+        System.out.println("Carregando dados do banco de dados.");
+    }
+
+}
---
+++ maratona-java/maratonajava/javacore/Minterfaces/test/DataLoaderTest1.java
@@ -0,0 +1,13 @@
+package maratonajava.javacore.Minterfaces.test;
+
+import maratonajava.javacore.Minterfaces.dominio.DatabaseLoader;
+import maratonajava.javacore.Minterfaces.dominio.FileLoader;
+
+public class DataLoaderTest1 {
+    public static void main(String[] args) {
+        DatabaseLoader databaseLoader = new DatabaseLoader();
+        FileLoader fileLoader = new FileLoader();
+        databaseLoader.load();
+        fileLoader.load();
+    }
+}
