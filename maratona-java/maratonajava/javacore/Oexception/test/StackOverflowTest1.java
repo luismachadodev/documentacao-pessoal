c0e56a10ead3a221e4610ad3f40b8f77a47b74b0
Luis Henrique <luis@prover.net.br>
2025-05-16 20:22:16 -0300

    feat: excecoes - erros

---
+++ maratona-java/maratonajava/javacore/Oexception/test/StackOverflowTest1.java
@@ -0,0 +1,11 @@
+package maratonajava.javacore.Oexception.test;
+
+public class StackOverflowTest1 {
+    public static void main(String[] args) {
+
+    }
+
+    public static void recursividade() {
+        recursividade();
+    }
+}
