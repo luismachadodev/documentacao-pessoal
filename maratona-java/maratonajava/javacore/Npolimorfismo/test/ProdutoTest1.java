bbe14d8c2363fcaa5a9a9786fc44202c4b629030
Luis Henrique <luis@prover.net.br>
2025-05-15 23:26:01 -0300

    polimorfismo - parametros polimorficos

--- maratona-java/maratonajava/javacore/Npolimorfismo/test/ProdutoTest1.java
+++ maratona-java/maratonajava/javacore/Npolimorfismo/test/ProdutoTest1.java
@@ -1,6 +1,7 @@
 package maratonajava.javacore.Npolimorfismo.test;
 
 import maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import maratonajava.javacore.Npolimorfismo.dominio.Televisao;
 import maratonajava.javacore.Npolimorfismo.dominio.Tomate;
 import maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
 
@@ -8,8 +9,12 @@ public class ProdutoTest1 {
     public static void main(String[] args) {
         Computador computador = new Computador("NUC10i7", 11000);
         Tomate tomate = new Tomate("Tomate Gauderio", 10);
-        CalculadoraImposto.calcularImpostoComputador(computador);
+        Televisao televisao = new Televisao("Samsung 50", 5000);
+
+        CalculadoraImposto.calcularImposto(computador);
         System.out.println("---------------");
-        CalculadoraImposto.calcularImpostoTomate(tomate);
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("---------------");
+        CalculadoraImposto.calcularImposto(televisao);
     }
 }
