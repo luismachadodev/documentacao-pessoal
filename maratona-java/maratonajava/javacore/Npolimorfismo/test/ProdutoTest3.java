d93019e3f1fc95b9afc5b672eca042f7c13a9604
Luis Henrique <luis@prover.net.br>
2025-05-16 19:46:34 -0300

    feat: polimorfismo - cast e instanceof

---
+++ maratona-java/maratonajava/javacore/Npolimorfismo/test/ProdutoTest3.java
@@ -0,0 +1,19 @@
+package maratonajava.javacore.Npolimorfismo.test;
+
+import maratonajava.javacore.Npolimorfismo.dominio.Computador;
+import maratonajava.javacore.Npolimorfismo.dominio.Produto;
+import maratonajava.javacore.Npolimorfismo.dominio.Tomate;
+import maratonajava.javacore.Npolimorfismo.servico.CalculadoraImposto;
+
+public class ProdutoTest3 {
+    public static void main(String[] args) {
+        Produto produto = new Computador("Ryzen 9", 3000);
+
+        Tomate tomate = new Tomate("Americano", 20);
+        tomate.setDataValidade("11/12/2021");
+
+        CalculadoraImposto.calcularImposto(tomate);
+        System.out.println("--------");
+        CalculadoraImposto.calcularImposto(produto);
+    }
+}
