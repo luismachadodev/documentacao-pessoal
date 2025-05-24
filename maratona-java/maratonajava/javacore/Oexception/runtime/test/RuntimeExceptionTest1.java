2fe39d7f9213cf3b6b3d18e27fb66ddf957fa04a
Luis Henrique <luis@prover.net.br>
2025-05-24 13:51:07 -0300

    feat: excecoes - runtimeexception

---
+++ maratona-java/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest1.java
@@ -0,0 +1,9 @@
+package maratonajava.javacore.Oexception.runtime.test;
+
+public class RuntimeExceptionTest1 {
+    public static void main(String[] args) {
+        // Checked e Unchecked
+        int[] nums = {1,2};
+        System.out.println(nums[2]);
+    }
+}
