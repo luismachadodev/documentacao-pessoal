2050b9c07c1eaa05c263e054cf2371f9f446b9ac
Luis Henrique <luis@prover.net.br>
2025-06-06 08:16:36 -0300

    feat: excecoes - multi catch em linha

--- maratona-java/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest4.java
+++ maratona-java/maratonajava/javacore/Oexception/runtime/test/RuntimeExceptionTest4.java
@@ -2,32 +2,29 @@ package maratonajava.javacore.Oexception.runtime.test;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.sql.SQLException;
 
 public class RuntimeExceptionTest4 {
     public static void main(String[] args) {
         try {
             throw new RuntimeException();
-        } catch (ArrayIndexOutOfBoundsException e) {
+        } catch (ArrayIndexOutOfBoundsException | IllegalArgumentException e) {
             System.out.println("Dentro do ArrayIndexOutOfBoundsException");
         } catch (IndexOutOfBoundsException e ) {
             System.out.println("Dentro do IndexOutOfBoundsException");
-        } catch (IllegalArgumentException e) {
-            System.out.println("Dentro do IllegalArgumentException");
         } catch (RuntimeException e) {
             System.out.println("Dentro do RuntimeException");
         }
 
         try {
             talvezLanceException();
-        } catch (SQLException throwables) {
-            throwables.printStackTrace();
-        } catch (FileNotFoundException e) {
+        } catch (IOException | SQLException e) {
             e.printStackTrace();
         }
     }
 
-    private static void talvezLanceException() throws SQLException, FileNotFoundException{
+    private static void talvezLanceException() throws SQLException, IOException {
 
     }
 }
