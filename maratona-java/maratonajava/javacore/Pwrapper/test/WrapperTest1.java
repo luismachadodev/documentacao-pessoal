067c1b8f2c634f1fa4bbe95c4a9806e1cd84b431
Luis Henrique <luis@prover.net.br>
2025-06-12 08:47:35 -0300

    feat: classes utilitarias - wrappers pt 2

--- maratona-java/maratonajava/javacore/Pwrapper/test/WrapperTest1.java
+++ maratona-java/maratonajava/javacore/Pwrapper/test/WrapperTest1.java
@@ -15,12 +15,23 @@ public class WrapperTest1 {
 
         Byte byteW = 1;
         Short shortW = 1;
-        Integer intW = 1;
+        Integer intW = 1; // autoboxing
         Long longW = 10L;
         Float floatW = 10F;
         Double doubleW = 10D;
         Character charW = 'L';
         Boolean booleanW = false;
 
+        int i = intW; //unboxing
+        Integer intW2 = Integer.parseInt("1");
+        boolean verdadeiro = Boolean.parseBoolean("truE");
+
+        System.out.println(Character.isDigit('A'));
+        System.out.println(Character.isDigit('9'));
+        System.out.println(Character.isLetterOrDigit('!'));
+        System.out.println(Character.isUpperCase('A'));
+        System.out.println(Character.isLowerCase('a'));
+        System.out.println(Character.toUpperCase('a'));
+        System.out.println(Character.toLowerCase('A'));
     }
 }
