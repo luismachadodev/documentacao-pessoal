fd1844e1611780d23f145b0f2ee52a5c04a2f908
Luis Henrique <luis@prover.net.br>
2025-06-12 09:00:14 -0300

    feat: classes utilitarias - strings

---
+++ maratona-java/maratonajava/javacore/Qstring/test/StringTest1.java
@@ -0,0 +1,14 @@
+package maratonajava.javacore.Qstring.test;
+
+public class StringTest1 {
+    public static void main(String[] args) {
+        String nome = "Luis"; //String constant pool
+        String nome2 = "Luis";
+        nome = nome.concat(" Henrique");
+        System.out.println(nome);
+        System.out.println(nome == nome2);
+        String nome3 = new String("Luis"); // 1 variavel de referencia, 2 objeto do tipo string, 3 uma string no pool de string
+        System.out.println(nome2 == nome3);
+        System.out.println(nome2 == nome3.intern());
+    }
+}
