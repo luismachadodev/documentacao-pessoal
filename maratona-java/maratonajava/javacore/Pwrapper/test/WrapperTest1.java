80b2f3f2d929b2588e3d2c7dc152c0410472132d
Luis Henrique <luis@prover.net.br>
2025-06-12 08:38:29 -0300

    feat: classes utilitarias - wrappers

---
+++ maratona-java/maratonajava/javacore/Pwrapper/test/WrapperTest1.java
@@ -0,0 +1,26 @@
+package maratonajava.javacore.Pwrapper.test;
+
+import java.util.List;
+
+public class WrapperTest1 {
+    public static void main(String[] args) {
+        byte byteP = 1;
+        short shortP = 1;
+        int intP = 1;
+        long longP = 10L;
+        float floatP = 10F;
+        double doubleP = 10D;
+        char charP = 'L';
+        boolean booleanP = false;
+
+        Byte byteW = 1;
+        Short shortW = 1;
+        Integer intW = 1;
+        Long longW = 10L;
+        Float floatW = 10F;
+        Double doubleW = 10D;
+        Character charW = 'L';
+        Boolean booleanW = false;
+
+    }
+}
